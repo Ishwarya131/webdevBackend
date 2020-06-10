61b3114bfc121cf4c50a0cda26cd76b53948f84c
Ishwarya A <63253438+Ishwarya131@users.noreply.github.com>
2020-06-10 14:42:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   port     :  '3306',
   user     : 'root',
-  password : 'Ai010301',
+  password : 'mysql123',
   database : 'mydb'
 });
 connection.connect(function(err){
